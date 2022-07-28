aafa0e882414f2e510c2b99307d07e3470ec40aa
reyel3 <reyel3@naver.com>
2022-07-28 17:47:51 +0900

    type script

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,57 @@
-import {init, exit} from "./myPackage"
+import crypto from "crypto"
 
-
-
-init({url:"url",id:0})
-exit(2)
-
-
-class Block {
-    constructor(
-        private data :string){}
-        static hello() {
-            return "hi"
-        }
-    
+interface BlockShape {
+    prevHash:string;
+    height:number
+    data:string
+    hash:string
 }
+
+class Block implements BlockShape{
+    public hash :string
+    constructor(
+        public prevHash : string,
+        public height : number,
+        public data : string
+    ) {
+        this.hash = Block.calculateHash(prevHash,height,data)
+    }
+
+    static calculateHash(prevHash:string, height:number, data:string){
+        const toHash =  `${prevHash}${height}${data}`
+        return crypto.createHash("sha256").update(toHash).digest("hex")
+    }
+
+
+}
+
+
+class BlockChain {
+    private blocks:Block[];
+    constructor() {
+        this.blocks = [];
+    }
+    private getPrevBlock() {
+        if(this.blocks.length === 0 ) return "";
+        return this.blocks[this.blocks.length-1].hash
+    }
+
+    public addBlock(data:string){
+        const newBlock = new Block(this.getPrevBlock(), this.blocks.length + 1, data)
+        this.blocks.push(newBlock)
+    }
+
+    public getBlock(){
+        // return this.blocks // 이렇게 리턴할경우 배열자체에 접근및 수정이 가능하기 때문에
+        return [...this.blocks] // 이런식으로 새로운 배열을 리턴해주는것이 보안상 더 좋다.
+    }
+
+}
+
+
+const block = new BlockChain()
+
+block.addBlock('first one')
+block.addBlock('second one')
+block.addBlock('third one')
+console.log(block.getBlock())
--- src/tmp.ts
+++
@@ -1,5 +0,0 @@
-import {init,exit} from "myPackage"
-
-init({url:"22"})
-
-exit(0)
